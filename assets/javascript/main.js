fc734028c0267d4676b6243fe708a9b48885bde9
pater97 <giuseppepater997@gmail.com>
2021-10-25 18:06:37 +0200

    fixed: all button can become blue at click

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,6 +10,7 @@ Un secondo array conterrà solo gli id dei posts a cui abbiamo dato like.
 //Creazione dell'array con i dati dei post all'interno
 const posts = [
     {
+        nUser: 1,
         id: `karot`,
         iconImage: `https://gametimers.it/wp-content/uploads/2015/12/dragon-ball-super.jpg`,
         contenuto: `Ho ancora fame! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere error maxime aperiam voluptas dolor officiis voluptatibus quasi, harum, magni vitae obcaecati consectetur corporis quam sequi accusamus! Aspernatur quaerat at explicabo!`,
@@ -18,6 +19,7 @@ const posts = [
         numeLike: `10`
     },
     {
+        nUser: 2,
         id: `vegeta`,
         iconImage: `https://gogomagazine.it/wp-content/uploads/2020/06/Veg-1280x720.jpg`,
         contenuto: `Sono il più forte! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere error maxime aperiam voluptas dolor officiis voluptatibus quasi, harum, magni vitae obcaecati consectetur corporis quam sequi accusamus! Aspernatur quaerat at explicabo!`,
@@ -26,6 +28,7 @@ const posts = [
         numeLike: `20`
     },
     {
+        nUser: 3,
         id: `crilin`,
         iconImage: `https://www.dragonballz.it/wp-content/uploads/2018/06/www-dragon-ball-z-crilin.jpg`,
         contenuto: `Aiuto Gokuuu! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere error maxime aperiam voluptas dolor officiis voluptatibus quasi, harum, magni vitae obcaecati consectetur corporis quam sequi accusamus! Aspernatur quaerat at explicabo!`,
@@ -34,6 +37,7 @@ const posts = [
         numeLike: `15`
     },
     {
+        nUser: 4,
         id: `junior`,
         iconImage: `https://4.bp.blogspot.com/-v0d4F7hBRSM/VZr2APQ4kiI/AAAAAAAAHBw/Am0_bVTmo3c/s1600/dbz5.png`,
         contenuto: `Ma io sono fortissimo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere error maxime aperiam voluptas dolor officiis voluptatibus quasi, harum, magni vitae obcaecati consectetur corporis quam sequi accusamus! Aspernatur quaerat at explicabo!`,
@@ -42,35 +46,46 @@ const posts = [
         numeLike: `80`
     }
 ]
+const likedPost = [1, 3]
+
+
 //selezionare una variabile per l'inserimento html
 const stamp = document.querySelector(`.container`)
 //ciclare l'arrey per estrapolare i valori
-for (let i = 0; i < posts.length; i++) {
-    const post = posts[i]
-    const element = `
-    <div class="card">
-    <div class="icon_container">
-        <img class="icon_image" onError="this.onerror=null;this.src='/images/noimage.gif';" src="${post.iconImage}"
-            alt="">
-        <div class="id_container">
-            <h3>${post.id}</h3>
-            <h6>${post.data}</h6>
+function stampAll() {
+    for (let i = 0; i < posts.length; i++) {
+        var post = posts[i]
+        const element = `
+        <div class="card">
+        <div class="icon_container">
+            <img class="icon_image" onError="this.onerror=null;this.src='/images/noimage.gif';" src="${post.iconImage}"
+                alt="">
+            <div class="id_container">
+                <h3>${post.id}</h3>
+                <h6>${post.data}</h6>
+            </div>
         </div>
-    </div>
-    <div class="content">
-        <p>
-        ${post.contenuto}
-        </p>
-        <img class="post_image" src="${post.immagine}" alt="">
-    </div>
-    <div class="button_counter">
-        <button class="like_button"><i class="fas fa-thumbs-up"></i>mi piace</button>
-        <div>piace a <span>${post.numeLike}</span> persone</div>
-    </div>
-</div>`
-    stamp.innerHTML += element
+        <div class="content">
+            <p>
+            ${post.contenuto}
+            </p>
+            <img class="post_image" src="${post.immagine}" alt="">
+        </div>
+        <div class="button_counter">
+            <button class="like_button"><i class="fas fa-thumbs-up"></i>mi piace</button>
+            <div>piace a <span>${post.numeLike}</span> persone</div>
+        </div>
+    </div>`
+        stamp.insertAdjacentHTML("beforeend", element)
+    }
 }
-const likeButton = document.querySelector(`.like_button`)
-likeButton.addEventListener(`click`, function () {
-    likeButton.classList.add("click_btn")
-})
+stampAll()
+const likeButton = document.getElementsByClassName(`like_button`)
+for (let index = 0; index < likeButton.length; index++) {
+    const singleButton = likeButton[index]
+    singleButton.addEventListener(`click`, function () {
+        singleButton.classList.add("click_btn")
+    })
+}
+
+//arrey post che han ricevuto mi piace, funzione i cui capisco se il bottone ha ricevuto il like, data post id attraverso il this
